3b288a3b1d0f33e0b80ae7e634a117b7a88d4e10
barnabeepickle <picklebarnabee@gmail.com>
2026-01-27 10:31:05 -0500

    fixed trans-mutation table by adding a new item for it

--- src/main/java/com/github/barnabeepickle/projectecharm/event/ModItemsEvent.java
+++ src/main/java/com/github/barnabeepickle/projectecharm/event/ModItemsEvent.java
@@ -1,19 +1,24 @@
 package com.github.barnabeepickle.projectecharm.event;
 
+import com.github.barnabeepickle.projectecharm.blocks.TransMutationTable;
 import com.github.barnabeepickle.projectecharm.items.TransMutationCharm;
 import com.github.barnabeepickle.projectecharm.items.TransMutationTablet;
 import com.github.barnabeepickle.projectecharm.items.TransmutationCharm;
+import moze_intel.projecte.gameObjs.items.itemBlocks.ItemTransmutationBlock;
 import net.minecraft.item.Item;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.registries.IForgeRegistry;
 
+import static com.github.barnabeepickle.projectecharm.event.ModBlocksEvent.*;
 import static com.github.barnabeepickle.projectecharm.utils.RegisteryUtilities.registerEntry;
 
 public class ModItemsEvent {
     public static TransmutationCharm TRANSMUTATION_CHARM = new TransmutationCharm();
     public static TransMutationCharm TRANS_MUTATION_CHARM = new TransMutationCharm();
     public static TransMutationTablet TRANS_MUTATION_TABLET = new TransMutationTablet();
+
+    public static ItemTransmutationBlock ITEM_TRANS_MUTATION_TABLE = new ItemTransmutationBlock(TRANS_MUTATION_TABLE);
 
     @SubscribeEvent
     public static void registerItemsEvent(RegistryEvent.Register<Item> event) {
@@ -24,5 +29,7 @@ public class ModItemsEvent {
         registerEntry(itemEvent, TRANS_MUTATION_CHARM, TransMutationCharm.getName());
         registerEntry(itemEvent, TRANS_MUTATION_TABLET, TransMutationTablet.getName());
 
+        // Register block items here
+        registerEntry(itemEvent, ITEM_TRANS_MUTATION_TABLE, TransMutationTable.getName());
     }
 }
